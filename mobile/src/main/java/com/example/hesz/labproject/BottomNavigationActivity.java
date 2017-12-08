0060541d28f0cb8cc56dacb12ba0f1ff26eeaa2b
heszgergely <heszgergely@gmail.com>
2017-12-08 14:59:42 +0100

    Final menu layout, added icons

--- mobile/src/main/java/com/example/hesz/labproject/BottomNavigationActivity.java
+++ mobile/src/main/java/com/example/hesz/labproject/BottomNavigationActivity.java
@@ -1,16 +1,21 @@
 package com.example.hesz.labproject;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class BottomNavigationActivity extends AppCompatActivity {
+import com.example.hesz.labproject.dummy.DummyContent;
+
+public class BottomNavigationActivity extends AppCompatActivity implements NodesFragment.OnFragmentInteractionListener, JobsFragment.OnFragmentInteractionListener, QueuesFragment.OnListFragmentInteractionListener{
 
     private TextView mTextMessage;
 
@@ -19,18 +24,38 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_jobs:
+                    selectedFragment =  JobsFragment.newInstance("param1","param1");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation_frame_container, selectedFragment).commit();
+                    break;
+                case R.id.navigation_nodes:
+                    selectedFragment = NodesFragment.newInstance("param1","param1");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation_frame_container, selectedFragment).commit();
+                    break;
+                case R.id.navigation_queues:
+                    selectedFragment = QueuesFragment.newInstance(4);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation_frame_container, selectedFragment).commit();
+                    break;
+            }
+            return true;
+
+
+         /*   switch (item.getItemId()) {
+                case R.id.navigation_jobs:
+                    mTextMessage.setText(R.string.title_jobs);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_nodes:
+                    mTextMessage.setText(R.string.title_nodes);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_queues:
+                    mTextMessage.setText(R.string.title_queues);
                     return true;
             }
-            return false;
+            return false;*/
         }
     };
 
@@ -51,6 +76,19 @@ public class BottomNavigationActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Fragment newFragment =  JobsFragment.newInstance("param1","param1");
+        getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation_frame_container, newFragment).commit();
+
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 }
---
+++ mobile/src/main/java/com/example/hesz/labproject/JobsFragment.java
@@ -0,0 +1,232 @@
+package com.example.hesz.labproject;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link JobsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link JobsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class JobsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public JobsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment JobsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static JobsFragment newInstance(String param1, String param2) {
+        JobsFragment fragment = new JobsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new JobsFragment.SectionsPagerAdapter(getChildFragmentManager());
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_jobs, container, false);
+
+
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = view.findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+
+        return view;
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return JobsFragment.PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "All";
+                case 1:
+                    return "Running";
+                case 2:
+                    return "Completed";
+                case 3:
+                    return "Other";
+            }
+
+            return null;
+        }
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static TabbedActivity.PlaceholderFragment newInstance(int sectionNumber) {
+            TabbedActivity.PlaceholderFragment fragment = new TabbedActivity.PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+}
